3b39258b173d2e3148e7e837c96f3401d59e955f
Kosi Akporji <kosiazom@gmail.com>
2020-04-17 18:40:24 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,10 +1,10 @@
 def find_max_value(array)
   count = 0 
-  max_value = -1
+  max_value = array.max
   
   while count < array.length do 
-    if max_value < array[count]
-      max_value = array[count]
+    if max_value == array.max
+      
 end
 count += 1
 end
